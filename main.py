60542ee7386b8794a42e0c62ba92e6be577cbcca
StavrosOrf <s.orfanoudakis@tudelft.nl>
2025-09-29 22:26:08 +0200

    Update device settings to use CUDA and adjust training parameters for experiments

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     # parser.add_argument("--env-id", type=str, default="MountainCarContinuous-v0")
     parser.add_argument("--env-id", type=str, default="Pendulum-v1")
     parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument("--device", type=str, default="cpu")    
+    parser.add_argument("--device", type=str, default="cuda")
 
     # wandb args
     parser.add_argument("--lightweight_wandb", action="store_false", default=True,
@@ -33,11 +33,11 @@ def main():
     parser.add_argument("--lr", type=float, default=3e-4)
     parser.add_argument("--hidden-size", type=int, default=64)
     parser.add_argument("--eval-episodes", type=int, default=2)
-    parser.add_argument("--policy-training-start", type=int, default=500)
-    parser.add_argument("--val-training-start", type=int, default=500)
+    parser.add_argument("--policy-training-start", type=int, default=1000)
+    parser.add_argument("--val-training-start", type=int, default=1000)
     parser.add_argument("--v_gamma", type=float, default=1.2)
     args = parser.parse_args()
-        
+
     exp_prefix = args.exp_prefix + "_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     group_name = args.env_id + "_test"
 
@@ -55,7 +55,7 @@ def main():
         if not args.lightweight_wandb:
             # wandb.run.log_code(".")
             wandb_run.log_code("distRL.py")
-            
+
         args.wandb_run = wandb_run
     else:
         args.wandb_run = None
--- runner.py
+++ runner.py
@@ -8,16 +8,18 @@ import time
 algo = "DistRL"
 device = "cuda"  # "cpu" or "cuda"
 
-for envs in ['Pendulum-v1', 'MountainCarContinuous-v0']:
+# for envs in ['Pendulum-v1', 'MountainCarContinuous-v0']:
+for envs in ['MountainCarContinuous-v0']:
     for batch_size in [128]:
-        for K in [2, 6, 15, 32, 64]:  # 512
+        for K in [2, 32]:  # 512
+        # for K in [2, 6, 15, 32, 64]:  # 512
             for v_gamma in [0.8, 1, 1.2]:
                 for lr in [3e-4]:
                     for hidden_size in [64]:
                         for seed in [42]:
                             name = f"{algo}-K{K}-v_gamma{v_gamma}-bs{batch_size}-lr{lr}-hs{hidden_size}-seed{seed}"
 
-                            command = 'tmux new-session -d \; send-keys " /home/sorfanouda/anaconda3/envs/klmn/bin/python main.py' + \
+                            command = 'tmux new-session -d \; send-keys "  /home/sorfanouda/anaconda3/envs/dt/bin/python main.py' + \
                                 f' --env-id {envs}' + \
                                 f' --algo {algo}' + \
                                 f' --device {device}' + \
